72b5ef28873208cd5e6266132dceb1544155aa0f
Marcus Parmentier <marcusjparmentier@gmail.com>
2020-03-29 15:40:40 -0500

    Created multidimensional quotes array

--- a_random_quote_generator-v1/inc/functions.php
+++ a_random_quote_generator-v1/inc/functions.php
@@ -1,6 +1,42 @@
+<?php
+
+
 // PHP - Random Quote Generator
 
-// Create the Multidimensional array of quote elements and name it quotes
+// multidimensional array named quotes
+
+$quotes[] = [
+    'quote' => "Motivation is crap. Motivation comes and goes. When you’re driven, whatever is in front of you will get destroyed.",
+    'source' => 'David Goggins',
+    'citation' => "Can't Hurt Me",
+    'year' => '2018',
+];
+$quotes[] = [
+    'quote' => "Our greatest glory is not in never falling, but in rising everytime we fall.",
+    'source' => 'Confucius',
+];
+$quotes[] = [
+    'quote' => "Suffering is discomfort multiplied by resistance.",
+    'source' => 'Buddhist saying',
+];
+$quotes[] = [
+    'quote' => "Success is not something that happens to you, it's something that happens because of you and because of the actions you take.",
+    'source' => 'Grant Cardone',
+    'citation' => 'The 10X Rule',
+    'year' => '2011',
+];
+$quotes[] = [
+    'quote' => "The art of living is more like wrestling than dancing.",
+    'source' => 'Marcus Aurelius',
+];
+$quotes[] = [
+    'quote' => "We work with nutrition and exercise to increase our energy, but we ignore the richest source of energy we possess-our emotions.",
+    'source' => 'Karla McLaren',
+    'citation' => 'The Language of Emotions',
+    'year' => '2010',
+];
+
+var_dump($quotes);
 // Each inner array element should be an associative array
 
 
@@ -10,3 +46,5 @@
 
 
 // Create the printQuote funtion and name it printQuote
+
+?>
